79d6b3fca8514e91305589c8da3c3186fc55abb4
Ryan A. Hagenson <Ryan.Hagenson@yale.edu>
2022-07-08 14:37:30 -0400

    Add R/ggplot2 procedure

---
+++ smash_viz.R
@@ -0,0 +1,79 @@
+#!/usr/bin/env Rscript
+
+suppressWarnings(suppressMessages(library(argparser, quietly = TRUE)))
+suppressWarnings(suppressMessages(library(dplyr, quietly = TRUE)))
+suppressWarnings(suppressMessages(library(ggplot2, quietly = TRUE)))
+suppressWarnings(suppressMessages(library(purrr, quietly = TRUE)))
+suppressWarnings(suppressMessages(library(stringr, quietly = TRUE)))
+
+# Create a parser
+p <- arg_parser("Produce a SMASH plot from results txt file")
+
+# Add command line arguments
+p <- add_argument(p, "--arms", help="Definitions of chromosomal arms")
+p <- add_argument(p, "--input", help="Points to plot")
+p <- add_argument(p, "--highlight", help="Chromosomal arm(s) to highlight; if multiple, separate with comma")
+p <- add_argument(p, "--color", help="The color to use for highlighting", default = "blue")
+p <- add_argument(p, "--title", help="The title to use for the resulting plot")
+p <- add_argument(p, "--output", help="The file to write the plot to")
+
+# Parse the command line arguments
+argv <- parse_args(p)
+
+arms <- read.csv(argv$arms, stringsAsFactors = FALSE)
+df <- read.csv(argv$input, stringsAsFactors = FALSE, sep = '\t')
+
+arms.clean <- arms %>%
+  mutate(chrom = ifelse(chrom == "X", 23, chrom)) %>%
+  mutate(chrom = ifelse(chrom == "Y", 24, chrom)) %>%
+  mutate(chrom = as.numeric(chrom)) %>%
+  arrange(chrom) %>%
+  mutate(end = cumsum(as.numeric(length))) %>%
+  mutate(chrom = ifelse(chrom == 23, "X", chrom)) %>%
+  mutate(chrom = ifelse(chrom == 24, "Y", chrom)) %>%
+  mutate(chrarm = paste0(chrom, arm)) %>%
+  mutate(start = lag(end, default = 0)) %>%
+  select(chrarm, start, end) %>%
+  mutate(across(c(start, end), as.numeric))
+
+df.clean <- df %>%
+  mutate(across(c(abspos, ratio, seg_ratio), as.numeric)) %>%
+  rowwise() %>% 
+  mutate(chrarm = arms.clean %>% rowwise() %>% filter(between(abspos, start, end)) %>% pluck("chrarm")) %>%
+  ungroup() %>%
+  mutate(ratio = ratio * 2,
+         seg_ratio = seg_ratio * 2,
+         abspos = abspos / 1e9) %>%
+  rowwise() %>% 
+  mutate(highlight = chrarm %in% unlist(str_split(argv$highlight, ","))) %>%
+  ungroup()
+  
+
+chrom.breaks <- df.clean %>% 
+  group_by(chr) %>% 
+  summarise(minmax = mean(abspos)) %>% 
+  pluck("minmax") %>% 
+  unlist() %>% 
+  sort()
+
+chrmom.lines <- df.clean %>% 
+  group_by(chr) %>% 
+  summarise(minmax = min(abspos)) %>% 
+  pluck("minmax") %>% 
+  unlist() %>% 
+  sort()
+
+
+ggplot(df.clean) + 
+  geom_point(data = df.clean %>% filter(!highlight), mapping = aes(x = abspos, y = ratio)) + 
+  geom_point(data = df.clean %>% filter(highlight), mapping = aes(x = abspos, y = ratio), color = argv$color) +
+  geom_line(aes(x = abspos, y = seg_ratio), color = 'red') + 
+  theme(legend.position="none") + 
+  geom_vline(data = data.frame(v = chrmom.lines), mapping = aes(xintercept = v), linetype = 'dashed') + 
+  labs(title = argv$title, x = "Genome Position (Gb)", y = "Copy Number") + 
+  scale_x_continuous(sec.axis = dup_axis(breaks = chrom.breaks, labels = c(1:22, "X", "Y"), name = NULL)) + 
+  theme_classic() + 
+  theme(axis.title.x = element_text(hjust=1), axis.title.y = element_text(hjust=1), axis.text = element_text(size = 12)) + 
+  ylim(0, 5)
+
+ggsave(argv$output, width = 16, height = 9)
